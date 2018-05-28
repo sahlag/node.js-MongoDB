78fd896c1af9f0a1f04465a5788dd77825ae0f9e
sahlag <boussaid-souheil@live.fr>
2018-05-28 12:13:22 +0200

    module http de node

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 // Appel du fichier “module1.js” dans le programme principal 
 const module1 = require('module1');
+const http = require('http')
 
+// test
 console.log('bonjour');
 const monTableau = [1, 2, 3, 4, 5];
 monTableau.forEach(elem => console.log(elem));
 
-console.log(module1)
+// Chargement module dans node_modules
+console.log(module1);
+
+// Module http de Node
+console.log(http);
 
 console.log('fin de l\'execution');
